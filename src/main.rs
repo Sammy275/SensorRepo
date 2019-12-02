214f90ec6cace2b9e8ef689e389aafade5e4da91
Saim Irfan <samfan95@gmail.com>
2019-12-02 17:53:49 +0500

    Updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 #[derive(Debug)]
 struct LightSensor {
     status: bool,
@@ -28,6 +30,28 @@ impl LightSensor {
 }
 
 fn main() {
-   let mut dev_1 = LightSensor {status: false, intensity: 0};
-
+    let mut dev_1 = LightSensor {status: false, intensity: 0};
+    let mut status = String::new();
+    println!("Do you want to turn on the light? type 'On'");
+    io::stdin().read_line(&mut status);
+    let status: String = status.trim().parse().unwrap();
+    match status {
+        On => dev_1.switch_off(),
+        _ => panic!("Please enter correct command"),
+    }
+    loop {
+        println!("");
+        println!("-------------------------------------");
+        println!("Type 'Increase' to increase intensity");
+        println!("Type 'Decrease' to decrease intensity");
+        println!("Type 'Off' to turn of the light");
+        println!("Type 'Check' to check the status of device");
+        let mut status = String::new();
+        io::stdin().read_line(&mut status);
+        let status: String = status.trim().parse().unwrap();
+        match status {
+            Increase => dev_1.double(),
+            Decrease => dev_1.dec(),
+        }    
+    }
 }
