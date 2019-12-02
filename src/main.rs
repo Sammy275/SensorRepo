ef8f6ac67a1d94d8119e4e5694dafb2a2e86da2e
Saim Irfan <samfan95@gmail.com>
2019-12-02 17:30:36 +0500

    added switching funcionality

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,20 @@ impl LightSensor {
     fn double(&mut self) {
         self.intensity = self.intensity + 2;
     }
+    fn switch_on(&mut self) {
+        self.status = true;
+        self.intensity = 2;
+    }
+    fn switch_off(&mut self) {
+        self.status = false;
+        self.intensity = 0;
+    }
 }
 
 fn main() {
-   let mut dev_1 = LightSensor {status: true, intensity: 2};
+   let mut dev_1 = LightSensor {status: false, intensity: 0};
    println!("{:?}", dev_1);
+   dev_1.switch_on();
    dev_1.double();
    println!("{:?}", dev_1);
 
