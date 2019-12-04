6532c219b68ca5f4d4c2cf0ccf3d92504e7399ec
Saim Irfan <samfan95@gmail.com>
2019-12-04 19:56:41 +0500

    updated lib

--- logic/src/lib.rs
+++ logic/src/lib.rs
@@ -1,7 +1,50 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[derive(Debug)]
+struct LightSensor {
+    status: bool,
+    intensity: u32,
+}
+struct FireAlarm {
+    status: bool,
+    intensity: u32,
+}
+struct GateAlarm {
+    status: bool,
+    intensity: u32,
+}
+
+pub trait Func {
+    fn double(&mut self);
+    fn dec(&mut self);
+    fn switch_on(&mut self);
+    fn switch_off(&mut self);
+    fn check(&mut self);
+}
+
+impl Func for LightSensor {
+    fn double(&mut self) {
+        self.intensity = self.intensity + 2;
+    }
+
+    fn dec(&mut self) {
+        self.intensity = self.intensity - 2;
+        if self.intensity == 0 {
+            self.status = false;
+            println!("Light turned off");
+        }
+    }
+
+    fn switch_on(&mut self) {
+        self.status = true;
+        self.intensity = 2;
+    }
+
+    fn switch_off(&mut self) {
+        self.status = false;
+        self.intensity = 0;
+    }
+
+    fn check(&mut self) {
+        println!("");
+        println!("Status = {}\nIntensity = {}", self.status, self.intensity);
     }
 }
