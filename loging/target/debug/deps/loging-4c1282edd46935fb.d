b273d17bb836d5bd6e7b5eefd9bfcbb62ead01c9
Saim Irfan <samfan95@gmail.com>
2019-12-05 19:53:06 +0500

    build loging library

---
+++ loging/target/debug/deps/loging-4c1282edd46935fb.d
@@ -0,0 +1,7 @@
+C:\Users\Zulfi\Desktop\struct\prac2\loging\target\debug\deps\loging-4c1282edd46935fb.rmeta: src\lib.rs
+
+C:\Users\Zulfi\Desktop\struct\prac2\loging\target\debug\deps\libloging-4c1282edd46935fb.rlib: src\lib.rs
+
+C:\Users\Zulfi\Desktop\struct\prac2\loging\target\debug\deps\loging-4c1282edd46935fb.d: src\lib.rs
+
+src\lib.rs:
---
+++ loging/target/debug/libloging.d
@@ -0,0 +1,1 @@
+C:\Users\Zulfi\Desktop\struct\prac2\loging\target\debug\libloging.rlib: C:\Users\Zulfi\Desktop\struct\prac2\loging\src\lib.rs
