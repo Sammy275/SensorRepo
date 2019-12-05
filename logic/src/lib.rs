a2613802bf646440c54962f2884730e9c22033bf
Saim Irfan <samfan95@gmail.com>
2019-12-05 16:38:12 +0500

    Updated lib.rs

--- logic/src/lib.rs
+++ logic/src/lib.rs
@@ -85,3 +85,63 @@ impl Func for FireAlarm {
         println!("Status = {}\nIntensity = {}", self.status, self.intensity);
     }
 }
+
+//For Gate Alarm
+impl Func for LightSensor {
+    fn double(&mut self) {
+        self.intensity = self.intensity + 2;
+    }
+
+    fn dec(&mut self) {
+        self.intensity = self.intensity - 2;
+        if self.intensity == 0 {
+            self.status = false;
+            println!("Light turned off");
+        }
+    }
+
+    fn switch_on(&mut self) {
+        self.status = true;
+        self.intensity = 2;
+    }
+
+    fn switch_off(&mut self) {
+        self.status = false;
+        self.intensity = 0;
+    }
+
+    fn check(&mut self) {
+        println!("");
+        println!("Status = {}\nIntensity = {}", self.status, self.intensity);
+    }
+}
+
+//For Fire Alarm
+impl Func for GateAlarm {
+    fn double(&mut self) {
+        self.intensity = self.intensity + 2;
+    }
+
+    fn dec(&mut self) {
+        self.intensity = self.intensity - 2;
+        if self.intensity == 0 {
+            self.status = false;
+            println!("Light turned off");
+        }
+    }
+
+    fn switch_on(&mut self) {
+        self.status = true;
+        self.intensity = 2;
+    }
+
+    fn switch_off(&mut self) {
+        self.status = false;
+        self.intensity = 0;
+    }
+
+    fn check(&mut self) {
+        println!("");
+        println!("Status = {}\nIntensity = {}", self.status, self.intensity);
+    }
+}
