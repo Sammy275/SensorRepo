6c3be8cf52e41ea3f8f16d38ebed4efe081dd626
Saim Irfan <samfan95@gmail.com>
2019-12-05 21:40:38 +0500

    updated lib.rs

--- loging/src/lib.rs
+++ loging/src/lib.rs
@@ -6,7 +6,7 @@ pub mod logs {
     
     
     // sign up functionality
-    pub fn signup()/* -> String */{
+    pub fn signup() -> String{
         println!("Enter your username");
         let mut username = String::new();
         io::stdin().read_line(&mut username)
@@ -17,9 +17,9 @@ pub mod logs {
             process::exit(1);
         }
         let filename = format!("{}.txt",username);
-        let filename2 = format!("{}light.txt",username);
-        let filename3 = format!("{}fire.txt",username);
-        let filename4 = format!("{}gate.txt",username);
+        // let filename2 = format!("{}light.txt",username);
+        // let filename3 = format!("{}fire.txt",username);
+        // let filename4 = format!("{}gate.txt",username);
         println!("Enter your password");
         let mut pass = String::new();
         io::stdin().read_line(&mut pass);
@@ -29,18 +29,18 @@ pub mod logs {
             process::exit(1);
         }
         File::create(&filename);
-        File::create(&filename2);
-        File::create(&filename3);
-        File::create(&filename4);
+        // File::create(&filename2);
+        // File::create(&filename3);
+        // File::create(&filename4);
         fs::write(&filename, pass)
             .expect("unable to write to file");
         login();
-        // return filename2;
+        return filename;
     }
 
 
     // log in functionality
-    pub fn login()/* -> String */{
+    pub fn login() -> String {
         println!("Please enter your username");
         let mut username = String::new();
         io::stdin().read_line(&mut username)
@@ -69,7 +69,7 @@ pub mod logs {
         else {
             println!("Access granted");
         }    
-        // filename2
+        filename
     }
 
 }
