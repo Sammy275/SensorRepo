54b4daf331ab95c609cf1f0e7d3d7b65932d701a
Saim Irfan <samfan95@gmail.com>
2019-12-05 16:16:31 +0500

    Updated lib.rs

--- logic/src/lib.rs
+++ logic/src/lib.rs
@@ -50,3 +50,33 @@ impl Func for LightSensor {
         println!("Status = {}\nIntensity = {}", self.status, self.intensity);
     }
 }
+
+//For Fire Alarm
+impl Func for FireAlarm {
+    fn double(&mut self) {
+        self.intensity = self.intensity + 2;
+    }
+
+    fn dec(&mut self) {
+        self.intensity = self.intensity - 2;
+        if self.intensity == 0 {
+            self.status = false;
+            println!("Light turned off");
+        }
+    }
+
+    fn switch_on(&mut self) {
+        self.status = true;
+        self.intensity = 2;
+    }
+
+    fn switch_off(&mut self) {
+        self.status = false;
+        self.intensity = 0;
+    }
+
+    fn check(&mut self) {
+        println!("");
+        println!("Status = {}\nIntensity = {}", self.status, self.intensity);
+    }
+}
